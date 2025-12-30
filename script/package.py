a19a1d6f35620f920bcb751e029cb65e6c9af653
Valentin Buira <valentin.buira@gmail.com>
2025-12-30 21:00:20 +0100

    improve packaging script * produce valide build by default * path agnostic

--- script/package.py
+++ script/package.py
@@ -1,18 +1,31 @@
 """
 Quick and dirty remplacement for pb_tool
 
-edit: for some reasons dezip and rezip the output to make it works
-
 """
 
 import configparser
+import os
+
+
+base_path = os.path.dirname(__file__)
+
+config_file_path = os.path.join(base_path, '../pb_tool.cfg')
+plugin_path = os.path.join(base_path, "../")
+zip_build_path = os.path.join(base_path, "../zip_build/")
+metadata_path = os.path.join(base_path, "../metadata.txt")
+
+
+if not os.path.exists(zip_build_path):
+    os.makedirs(zip_build_path)
 
 config = configparser.ConfigParser()
+config.read(config_file_path)
 
-config.read('../pb_tool.cfg')
-
+metadata = configparser.ConfigParser()
+metadata.read(metadata_path)
 
 plugin_name_dir = config["plugin"]["name"]
+plugin_version = metadata["general"]["version"]
 
 file_names = []
 for cat in config["files"]:
@@ -31,17 +44,17 @@ def compress(file_names):
     print("File Paths:")
     print(file_names)
 
-    output_path = "../"
-
     # Select the compression mode ZIP_DEFLATED for compression
     # or zipfile.ZIP_STORED to just store the file
     compression = zipfile.ZIP_DEFLATED
 
     # create the zip file first parameter path/name, second mode
-    zf = zipfile.ZipFile(f"{plugin_name_dir}.zip", mode="w")
+    zf = zipfile.ZipFile(os.path.join(zip_build_path, f"{plugin_name_dir}_v{plugin_version}.zip"), mode="w")
     try:
         for file_name in file_names:
-            zf.write(output_path + file_name, file_name, compress_type=compression)
+            zf.write(plugin_path + file_name,
+                     arcname=plugin_name_dir +"/" + file_name,
+                     compress_type=compression)
 
     except FileNotFoundError as e:
         print("An error occurred:", e)
